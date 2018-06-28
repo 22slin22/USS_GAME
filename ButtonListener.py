25733db1f43030578dea6fe218a19d5a7fb8a94c
Raspberry <nils-cremer@t-online.de>
2018-06-28 14:09:08 +0000

    buttons raspi

--- ButtonListener.py
+++ ButtonListener.py
@@ -1,13 +1,13 @@
-# from RPi import GPIO
+from RPi import GPIO
 import time
 
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(10, GPIO.IN)
-# GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN)
+GPIO.setwarnings(False)
 
 
 class ButtonListener:
-    time_click_gap = 0.5
+    time_click_gap = 0.2
     time_last_click = 0
     state = False
 
@@ -18,19 +18,15 @@ class ButtonListener:
         self.frame_manager.bind("<Down>", lambda event, button_index=1: self.button_pressed(button_index))
         self.frame_manager.bind("<Right>", lambda event, button_index=2: self.button_pressed(button_index))
 
-        # self.frame_manager.frames["StartPage"].bind("")
-
     def check_buttons(self):
-        '''
         if GPIO.input(10) and self.state == False:
-            state = True
+            self.state = True
             if time.monotonic() - self.time_last_click > self.time_click_gap:
-                print("clicked")
+                self.button_pressed(0)
                 self.time_last_click = time.monotonic()
     
         if not GPIO.input(10):
             self.state = False
-        '''
 
     def button_pressed(self, button_index):
         self.frame_manager.on_button_pressed(button_index)
