1195fde3ac59aa696f6c99c22a61d94960155444
Raspberry <nils-cremer@t-online.de>
2018-07-06 11:51:46 +0000

    button-info

---
+++ Utils.py
@@ -0,0 +1,18 @@
+button_width = 30
+button_height = 30
+
+button_delta_y = 90
+button_delta_x = 30
+
+button_gap = 10
+
+def draw_button_info(canvas, text0, text1, text2):
+    width = canvas.winfo_screenwidth()
+    height = canvas.winfo_screenheight()
+    
+    canvas.create_rectangle(width - button_delta_x - 3*button_width - 2*button_gap, height - button_delta_y - button_height,
+                            width - button_delta_x - 2*button_width - 2*button_gap, height - button_delta_y)
+    canvas.create_rectangle(width - button_delta_x - 2*button_width - button_gap, height - button_delta_y - button_height,
+                            width - button_delta_x - 1*button_width - button_gap, height - button_delta_y)
+    canvas.create_rectangle(width - button_delta_x - button_width, height - button_delta_y - button_height,
+                            width - button_delta_x, height - button_delta_y)
